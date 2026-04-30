fce733048e0bbdd067399cc734d81652a772c4f8
Abhilash-K-R <abhilashkr203@gmail.com>
2026-04-30 13:44:55 +0530

    added Day 1 work

---
+++ assignment/day1.py
@@ -0,0 +1,16 @@
+def abhi(arr):
+    odd_sum = 0
+    even_sum = 0
+
+    for i in range(len(arr)):
+        if i % 2 == 0:
+            even_sum += arr[i]
+        else:
+            odd_sum += arr[i]
+
+    if even_sum > odd_sum:
+        return even_sum - odd_sum
+    else:
+        return odd_sum - even_sum
+
+print(abhi([4,6,1,3,8]))
